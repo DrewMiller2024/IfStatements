0b832cfb2bab3914a885e4dc983c86e6f6de0a36
Drew Miller <drew.miller@mrjaffesclass.com>
2022-09-22 10:57:18 -0700

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class IfStatements here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        System.out.println("Hello World!");
+    }
+    
+    
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+}
+
